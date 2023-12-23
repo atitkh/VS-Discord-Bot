6463e2d65d07e106b1513912ca08a1145ace7978
Atit Kharel <atit286@gmail.com>
2023-12-23 22:00:29 +0545

    Refactor token handling in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const { prefix } = require('./config.json');
-const token = process.env['token']
-
+require('dotenv').config();
+const token = process.env.TOKEN
 const bot = new Discord.Client({
   partials: ['MESSAGE', 'REACTION']
 });
